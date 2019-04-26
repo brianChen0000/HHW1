ac939c0d9f95a1907bf9fcd0dd10a2b46e2a8be4
brianChen0000 <50001902+brianChen0000@users.noreply.github.com>
2019-04-26 11:12:32 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hard1
+//
+//  Created by Mac on 2019/4/12.
+//  Copyright © 2019年 Mac. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+int main ()
+{
+    
+    for (int i = 0 ; i <26 ; i ++ )
+        cout << (char) ('A'+i);
+    
+    return 0 ; // complete !
+    
+}
